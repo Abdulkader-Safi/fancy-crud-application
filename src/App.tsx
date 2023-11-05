d7a0531bd7fe3ae171aa49bdf94e3f50660dad1d
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-11-05 13:55:41 +0200

    Design Product Card UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import { useState } from "react";
-import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import ProductCard from "./components/ProductCard";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className="text-3xl font-bold underline">Vite + React!</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+    <div className="m-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2 p-2 rounded-md">
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+      <ProductCard />
+    </div>
   );
 }
 
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,46 @@
+interface IProps {}
+
+// eslint-disable-next-line no-empty-pattern
+const ProductCard = ({}: IProps) => {
+  return (
+    <div className="border rounded-md p-2 flex flex-col">
+      <img
+        src="https://images.unsplash.com/photo-1502877338535-766e1452684a?auto=format&fit=crop&q=80&w=1744&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        alt="Product Name"
+      />
+
+      <h3>Blue BMW</h3>
+
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde consectetur ex quis, recusandae aspernatur autem
+      </p>
+
+      <div className="flex items-center my-4 space-x-2">
+        <span className="w-5 h-5 bg-indigo-300 rounded-full cursor-pointer" />
+        <span className="w-5 h-5 bg-blue-300 rounded-full cursor-pointer" />
+        <span className="w-5 h-5 bg-yellow-300 rounded-full cursor-pointer" />
+        <span className="w-5 h-5 bg-red-300 rounded-full cursor-pointer" />
+      </div>
+
+      <div className="flex items-center justify-between">
+        <span>$500,000</span>
+        <img
+          src="https://images.unsplash.com/photo-1502877338535-766e1452684a?auto=format&fit=crop&q=80&w=1744&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          alt="Product Name"
+          className="w-10 h-10 rounded-full"
+        />
+      </div>
+
+      <div className="flex items-center justify-between space-x-1 mt-5">
+        <button type="submit" className="bg-indigo-700 p-2 flex-1 rounded-md text-white">
+          Edit
+        </button>
+        <button type="submit" className="bg-red-700 p-2 flex-1 rounded-md text-white">
+          Destroy
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
