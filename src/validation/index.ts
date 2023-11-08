ac6913425527ae7e102d06ca5a7be6501a68a39d
Abdulkader Safi <safi.abdulkader@gmail.com>
2023-11-08 20:29:30 +0200

    finish working on input validation

--- src/validation/index.ts
+++ src/validation/index.ts
@@ -1,17 +1,19 @@
-interface IInputType {
+interface productValidationType {
   title: string;
   description: string;
   imageURL: string;
   price: string;
 }
 
-export const productValidation = (product: IInputType) => {
-  const errors: IInputType = {
+export const productValidation = (product: productValidationType) => {
+  const errors: productValidationType = {
     title: "",
     description: "",
     imageURL: "",
     price: "",
   };
+
+  const validUrl = /^(ftp|http|https):\/\/[^ "]+$/.test(product.imageURL);
 
   if (!product.title.trim() || product.title.length < 10 || product.title.length > 80) {
     errors.title = "Product title must be between 10 and 80 characters!";
@@ -19,6 +21,14 @@ export const productValidation = (product: IInputType) => {
 
   if (!product.description.trim() || product.description.length < 10 || product.description.length > 900) {
     errors.description = "Product title must be between 10 and 900 characters!";
+  }
+
+  if (!product.imageURL.trim() || !validUrl) {
+    errors.imageURL = "Valid Image URL is required!";
+  }
+
+  if (!product.price.trim() || !isNaN(Number(product.price))) {
+    errors.price = "Valid Price is required!";
   }
 
   return errors;
